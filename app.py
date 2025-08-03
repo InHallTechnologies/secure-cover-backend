2ea92177a5e898dc6d37a68702ac75ce06c841d2
Rishabh Verma <rishabhverma@Rishabhs-MacBook-Pro.local>
2025-08-03 23:03:50 +0530

    Reporting update

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from blueprints.user_blueprints import user_blueprints
+from blueprints.report_blueprints import report_blueprint
 from firebase_admin import initialize_app, credentials, db
-from flask_cors import CORS
 
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
@@ -10,10 +11,13 @@ CORS(app)
 cred = credentials.ApplicationDefault()
 initialize_app(cred, {"databaseURL": "https://cover-6c458-default-rtdb.firebaseio.com"})
 app.register_blueprint(user_blueprints)
+app.register_blueprint(report_blueprint)
+
 
 @app.route("/", methods=["GET"])
 def handle_test():
     return db.reference("/USER_ARCHIVE/0jYBHEIRPmZQWC8Nj0gGVQSVFiN2/name").get()
+    
 
 if __name__ == "__main__":
     app.run("0.0.0.0", 8080, debug=True)
---
+++ blueprints/report_blueprints.py
@@ -0,0 +1,60 @@
+from flask import Blueprint, request, send_file
+from firebase_admin import db
+from constants.admin_uids import admin_uid
+import pandas as pd
+import numpy as np
+from io import BytesIO
+
+report_blueprint = Blueprint('reporting_blueprint',__name__, url_prefix="/reporting")
+
+@report_blueprint.route("/call_report", methods=["POST"])
+def download_call_logs():
+    print("Hello")
+    arguments = request.get_json(silent=True)
+    request_uid = request.headers.get("X-Admin-Uid", "")
+
+    if request_uid not in admin_uid:
+        return {"message": "Unauthorized", "status": 401}, 401
+    if not arguments:
+        return {"message": "Bad Request", "status": 400}, 400
+
+    date = arguments.get("date", None)
+    date = date.split("-")[::-1]
+    date = "-".join(date)
+    print(date)
+    if not date:
+        return {"message": "Bad Request", "status": 400}, 400
+    
+    users = db.reference("/USER_ARCHIVE").get()
+    users_list = list(users.keys())
+
+    data = []
+    for uid in users_list:
+        call_logs = db.reference("/NEW_CALL_LOGS").child(uid).child(date).get()
+        if call_logs:
+            call_list = list(call_logs.values())
+            new_list = []
+            for log in call_list:
+                log['advisorName'] = users[uid]['name']
+                new_list.append(log)
+
+            data = data + new_list
+
+
+    call_logs_dataframe = pd.DataFrame.from_records(data)
+    call_logs_dataframe.replace(np.nan, None, inplace=True)
+
+    buf = BytesIO()
+    with pd.ExcelWriter(buf, engine="openpyxl") as writer:
+        call_logs_dataframe.to_excel(writer, index=False, sheet_name=f"calling_report_{date}")
+    buf.seek(0)
+    print("COmpleteed")
+    call_logs_dataframe.to_excel("./temp.xlsx")
+    return send_file(
+        buf,
+        as_attachment=True,
+        download_name=f"calling_report_{date}.xlsx",
+        mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
+
+    
